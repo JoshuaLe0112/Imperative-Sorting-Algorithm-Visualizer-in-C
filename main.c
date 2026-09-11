37ea62ade7a4ec5469a6008b5e9ec0c5ecd9ae0f
Quan Le <qlehoang@nmu.edu>
2026-09-10 21:15:36 -0400

    Adding program input function for user

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void inputOption(char option[], char input[]);
+
 int main (int argc, char **argv){
 
-    printf("Hello From Quan");
+    char option[100];
+    char input[100];
+   
+    inputOption(option,input);
 
     return 0;
+}
+
+//Program Input
+void inputOption(char option[], char input[]){
+
+    printf("Enter bubble, selection, or insertion: ");    
+    scanf("%s",option);
+    printf("Enter the characters to be sorted: ");
+    scanf("%s",input);
+
+    printf("Check the option: %s",option);
+    printf("\nCheck the sort option: %s\n",input);
+    
+
 }
